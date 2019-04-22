d83f9eae6b110fd77b3caa373cc25da81d4291b4
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2019-04-22 11:39:50 +0530

    Create size-of-binary-tree.cpp

---
+++ size-of-binary-tree.cpp
@@ -0,0 +1,88 @@
+// https://practice.geeksforgeeks.org/problems/size-of-binary-tree/1
+
+{
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Computes the number of nodes in a tree. */
+int getSize(struct Node* node);
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << getSize(root) << endl;
+  }
+  return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left;
+     Node* right;
+}; */
+/* Computes the number of nodes in a tree. */
+int getSize(Node* node)
+{
+   // Your code here
+   
+   if(node == NULL){
+       return 0;
+   }
+   return (getSize(node->left) + getSize(node->right) + 1);
+}
