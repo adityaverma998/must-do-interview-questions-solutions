caa2801f3944053a00e8931aa846c03ce9ec4e9c
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2019-04-24 21:52:10 +0530

    Create diameter-of-a-binary-tree.cpp

---
+++ diameter-of-a-binary-tree.cpp
@@ -0,0 +1,107 @@
+
+//       https://www.geeksforgeeks.org/diameter-of-a-binary-tree/
+
+
+{
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Function to get diameter of a binary tree */
+int diameter(struct Node * tree);
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << diameter(root) << endl;
+  }
+  return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+/* Computes the diameter of binary tree with given root.  */
+
+int max(int a,int b){
+    if(a>b){
+        return a;
+    }
+    return b;
+}
+
+int height(struct Node * r){
+    if(r==NULL){
+        return 0;
+    }
+    return 1 + max(height(r->left),height(r->right));
+}
+
+int diameter(Node* node)
+{
+   // Your code here
+   
+   if(!node){
+       return 0;
+   }
+   int lheight = height(node->right);
+   int rheight = height(node->left);
+   int x = max(diameter(node->left),diameter(node->right));
+   return max(x,1+lheight+rheight);
+}
