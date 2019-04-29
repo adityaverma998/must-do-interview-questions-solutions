18cc8551dd04bca2b7349fbc704005a6f192f839
Aditya Verma <39986925+adityaverma998@users.noreply.github.com>
2019-04-30 00:11:54 +0530

    Create convert-a-given-tree-to-sum-tree.cpp

---
+++ convert-a-given-tree-to-sum-tree.cpp
@@ -0,0 +1,157 @@
+//     https://www.geeksforgeeks.org/convert-a-given-tree-to-sum-tree/
+
+//        http://eclassesbyravindra.com/mod/page/view.php?id=6293
+
+
+
+{
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+void toSumTree(struct Node *node);
+// A utility function to print inorder traversal of a Binary Tree
+void printInorder(struct Node* node)
+{
+     if (node == NULL)
+          return;
+     printInorder(node->left);
+     printf("%d ", node->data);
+     printInorder(node->right);
+}
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     toSumTree(root);
+     printInorder(root);
+     printf("
+");
+  }
+  return 0;
+}
+/* Utillity function to check if the given node is leaf or not */
+int isLeaf(struct Node *node)
+{
+    if(node == NULL)
+        return 0;
+    if(node->left == NULL && node->right == NULL)
+        return 1;
+    return 0;
+}
+/* returns 1 if SumTree property holds for the given
+    tree */
+bool isSumTree(struct Node* node)
+{
+    int ls; // for sum of nodes in left subtree
+    int rs; // for sum of nodes in right subtree
+    /* If node is NULL or it's a leaf node then
+       return true */
+    if(node == NULL || isLeaf(node))
+        return 1;
+    if( isSumTree(node->left) && isSumTree(node->right))
+    {
+        // Get the sum of nodes in left subtree
+        if(node->left == NULL)
+            ls = 0;
+        else if(isLeaf(node->left))
+            ls = node->left->data;
+        else
+            ls = 2*(node->left->data);
+        // Get the sum of nodes in right subtree
+        if(node->right == NULL)
+            rs = 0;
+        else if(isLeaf(node->right))
+            rs = node->right->data;
+        else
+            rs = 2*(node->right->data);
+        /* If root's data is equal to sum of nodes in left
+           and right subtrees then return 1 else return 0*/
+        return(node->data == ls + rs);
+    }
+    return 0;
+}
+
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+// Convert a given tree to a tree where every node contains sum of values of
+// nodes in left and right subtrees in the original tree
+
+int issum(struct Node * r){
+    if(!r){
+        return 0;
+    }
+    int left = issum(r->left);
+    int right = issum(r->right);
+    
+    int d = r->data;
+    r->data = left + right;
+    
+    return (d + r->data);
+}
+
+void toSumTree(Node *node)
+{
+    // Your code here
+    
+    issum(node);
+    return;
+    
+}
